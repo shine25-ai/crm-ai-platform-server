5dd0d085e2daf6b66c29f70ccf47e526826b7df0
Jawahar Ganesh Raja <jawahar.as@shinelogics.com>
2026-06-11 18:36:43 +0530

    chore: comment out department and employee seeder import in server startup

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const app = require('./app');
 const connectDB = require('./config/database');
 const createDefaultRole = require('./seeders/roleSeeder.js');
 const createDefaultAdmin = require('./seeders/superAdminSeeder.js');
-const seedDepartmentsAndEmployees = require('./seeders/departmentAndEmployeeSeeder.js');
+// const seedDepartmentsAndEmployees = require('./seeders/departmentAndEmployeeSeeder.js');
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
